2a497f4ea979d194be26886100b5906336a3b85b
kenshineo <kenshineo@gmail.com>
2023-04-24 23:07:03 -0300

    new file:   logica_Udemy.py

---
+++ logica_Udemy.py
@@ -0,0 +1,14 @@
+#Logica Basica
+#Variaveis
+varInteiro = 10
+varReal = 2.50989
+varBoolean = True
+varString = "Reiniciando a lógica!"
+#Entrada - Do usuario para a maquina
+#Neste exemplo o programa faz uma leitura do teclado e armazena em uma variável string de nome "nome1"
+nome1 = input("Digite seu nome: ")
+#Saida - Da maquina para o usuario
+#Neste exemplo o progrma mostra na tela o conteúdo da variável "nome1"
+print('-------------------------')
+print(nome1)
+print('-------------------------')
