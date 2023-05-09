f6267b77496e87542c048656a2594321fc8c1c2e
kenshineo <kenshineo@gmail.com>
2023-05-08 22:51:37 -0300

    modified:   logica_Udemy_Exerc_Cap05.py

--- logica_Udemy_Exerc_Cap05.py
+++ logica_Udemy_Exerc_Cap05.py
@@ -12,7 +12,6 @@ def exerc1():
             print('DECRESCENTE!')
         else:
             print('CRESCENTE!')
-
         num1 = int(input('Número 1: '))
         num2 = int(input('Número 2: '))
 
@@ -20,22 +19,18 @@ def exerc2():
     #Exercício 2 - MEDIA IDADES - Faça um programa para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. 
     #   O último dado, que não entrará nos cálculos, contém um valor de idade negativa. 
     #   Calcular e imprimir a idade média deste grupo de indivíduos. Se for entrado um valor negativo na primeira vez, mostrar a mensagem "IMPOSSIVEL CALCULAR".
-    somaIdades = 0
-    n = 0
+    n = soma = 0
     media = 0.0
     idades = int(input('Digite as idades: '))
-    if idades < 0:
-        print('IMPOSSIVEL CALCULAR')
+    while idades >= 0:
+        n += 1
+        soma += idades
+        idades = int(input('Digite as idades: '))
+    if n > 0:
+        media = soma / n
+        print(f'MEDIA = {media:.2f}')
     else:
-        while idades >= 0:
-            n += 1
-            media += idades
-            idades = int(input('Digite as idades: '))
-
-        media2 = media / n
-        media /= n
-        if n > 0:
-            print(f'MEDIA = {media:.2f}')
+        print('IMPOSSIVEL CALCULAR')
 
 def exerc3():
     #Exercício 3 - SENHA FIXA - Escreva um programa que repita a leitura de uma senha até que ela seja válida. 
@@ -44,20 +39,16 @@ def exerc3():
     #   Considere que a senha correta é o valor 2002.
     SENHA_CORRETA = 2002
     senha = int(input('Digite a senha: '))
-
     while senha != SENHA_CORRETA:
         senha = int(input('Senha Inválida! Tente novamente: '))
-
     print('Acesso Permitido!')
 
 def exerc4():
     #Exercício 4 - QUADRANTE - Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. 
     #   Para cada ponto escrever o quadrante a que ele pertence (Q1, Q2, Q3 ou Q4). O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA.
     #  (nesta situação sem escrever mensagem alguma)
-    cordenadaX = 1
-    cordenadaY = 1
+    cordenadaY = cordenadaX = 1
     quadrante = ''
-
     while cordenadaX != 0 and cordenadaY != 0:
         cordenadaX = int(input('Digite o valor da coordenada X: '))
         cordenadaY = int(input('Digite o valor da coordenada Y: '))        
@@ -76,15 +67,16 @@ def exerc5():
     #Exercício 5 - VALIDAÇÃO DE NOTA - Faça um programa que leia as notas referentes às duas avaliações de um aluno. Calcule e imprima a média semestral. 
     #   Faça com que o algoritmo só aceite notas válidas (uma nota válida deve pertencer ao intervalo [0,10]). Cada nota deve ser validada separadamente. 
     media = 0.0
-    valor1 = float(input('Digite a primeira nota: '))
-    while valor1 < 0 or valor1 > 10:
-        valor1 = float(input('Valor inválido! Tente novamente: '))
-    valor2 = float(input('Digite a segunda nota: '))
-    while valor2 < 0 or valor2 > 10:
-        valor2 = float(input('Valor inválido! Tente novamente: '))
-    media = (valor1 + valor2) / 2    
-
-    print(f'MEDIA = {media:.2f}')
+    for i in range(2):
+        valor1 = float(input('Digite a primeira nota: '))
+        while valor1 < 0 or valor1 > 10:
+            valor1 = float(input('Valor inválido! Tente novamente: '))
+        valor2 = float(input('Digite a segunda nota: '))
+        while valor2 < 0 or valor2 > 10:
+            valor2 = float(input('Valor inválido! Tente novamente: '))
+        media = (valor1 + valor2) / 2
+        i += 1
+        print(f'MEDIA = {media:.2f}')
 
 def exerc6():
     #Exercício 6 - COMBUSTÍVEL - Um posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes.
@@ -100,12 +92,11 @@ def exerc6():
             gasolina += 1
         elif codigo == 3:
             diesel += 1
-
     print('MUITO OBRIGADO')
     print(f'Alcool: {alcool}\nGasolina: {gasolina}\nDiesel: {diesel}')
 
 def exerc7():
-    #Exercício 7 - PARES CPMSECUTIVOS - O  programa deve ler um valor inteiro X indefinidas vezes. (O programa irá parar quando o valor de X for igual a 0). 
+    #Exercício 7 - PARES CONSECUTIVOS - O  programa deve ler um valor inteiro X indefinidas vezes. (O programa irá parar quando o valor de X for igual a 0). 
     #   Para cada X lido, imprima a soma dos 5 pares consecutivos a partir de X, inclusive o X, se for par. 
     #   Se o valor de entrada for 4, por exemplo, a saída deve ser 40, que é o resultado da operação: 4+6+8+10+12, enquanto que se o valor de entrada for 11, 
     #   por exempo, a saída deve ser 80, que é a soma de 12+14+16+18+20.
@@ -120,17 +111,16 @@ def exerc7():
                 i += 1        
             else:
                 x += 1
-
         print(f'SOMA = {soma}, i = {i}, x = {x}')
         x = int(input('Digite um número interiro: '))
 
 def exerc8():
     #Exercício 8 - TABUADA - Ler um número inteiro N, daí mostrar na tela a tabuada de N para 1 a 10. 
     numero = int(input('Deseja a tabuada para qual valor? '))
-    i = 1
-    while i <= 10:
-        print(f'{numero} x {i:2} = {numero * i}')
+    for i in range(10):
         i += 1
+        produto = i * numero
+        print(f'{numero} x {i:2} = {produto}')
 
 def exerc9():
     #Exercício 9 - SOMA IMPARES - Leia 2 valores inteiros X e Y (em qualquer ordem). 
@@ -138,38 +128,25 @@ def exerc9():
     x = int(input('Digite o valor do número X: '))
     y = int(input('Digite o valor do número Y: '))
     acumulador = 0
-
-    #Verificando qual dos dois termos é o maior para tirar a diferença
+    #Verificando qual dos dois termos é o maior para definir a ordem crescente obrigatoriamente, sendo X o menor e Y o maior
     if x > y:
-        maior = x
-        menor = y
-    else:
-        maior = y    
-        menor = x
-    
-    #Removendo as pontas, pois o exercício quer o cálculo dos valores "entre" os termos
-    if maior % 2 == 0:
-        maior -= 1
-    else:
-        maior -= 2
-    if menor % 2 == 0:
-        menor += 1
-    else:
-        menor += 2
-
-    while menor <= maior:
-        acumulador += menor
-        menor += 2
-
+        aux = x
+        x = y
+        y = aux
+    x += 1
+    y -= 1
+    while x <= y:
+        if x % 2 != 0:
+            acumulador += x
+        x += 1
     print(f'SOMA DOS IMPARES = {acumulador}')
 
 def exerc10():
     #Exercício 10 - SEQUENCIA IMPARES - Leia um valor inteiro X. Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
     x = int(input('Digite o valor de X: '))
-    i = 1
-    while i <= x:
-        if i % 2 != 0:
-            print(i)
+    for i in range(x):
+        if (i + 1) % 2 != 0:
+            print(i + 1)
         i += 1
         
 def exerc11():
@@ -177,15 +154,12 @@ def exerc11():
     #   Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo.
     qtdNum = int(input('Quantos números você vai digitar? '))
     dentro = fora = 0
-    while qtdNum > 0:
+    for i in range(qtdNum):
         num = int(input('Digite um número: '))
         if num >= 10 and num <= 20:
             dentro += 1
         else:
             fora += 1
-        
-        qtdNum -= 1
-
     print('VERIFICANDO QUANTOS DOS NÚMEROS DIGITADOS ESTÃO DENTRO DO INTERVALO [10,20]')
     print(f'{dentro} DENTRO')
     print(f'{fora} FORA')
@@ -195,7 +169,7 @@ def exerc12():
     #   Para cada valor lido, mostre uma mensagem dizendo se este valor lido é PAR ou IMPAR, e também se é POSITIVO ou NEGATIVO. 
     #   No caso do valor ser igual a zero (0), seu programa deverá imprimir apenas NULO. 
     qtdNum = int(input('Quantos números você vai digitar? '))
-    while qtdNum > 0:
+    for i in range(qtdNum):
         num = int(input('Digite um número: '))
         if num == 0:
             print('NULO')
@@ -209,8 +183,6 @@ def exerc12():
                 print('PAR NEGATIVO')
             else:
                 print('IMPAR NEGATIVO')
-
-        qtdNum -= 1
         
 def exerc13():
     #Exercício 13 - MEDIA PONDERADA - Leia um valor inteiro N, que representa o número de casos de teste que vem a seguir. 
@@ -221,12 +193,11 @@ def exerc13():
     PESO1 = 2
     PESO2 = 3
     PESO3 = 5
-    while n > 0:
+    for i in range(n):
         print('Digite três números: ')
         valor1 = float(input())
         valor2 = float(input())
         valor3 = float(input())
-        n -= 1
         mediaPonderada = (valor1 * PESO1 + valor2 * PESO2 + valor3 * PESO3) / (PESO1 + PESO2 + PESO3)
         print(f'MEDIA = {mediaPonderada:.1F}')
 
@@ -234,7 +205,7 @@ def exerc14():
     #Exercício 14 - DIVISAO - Escreva um algoritmo que leia dois números e imprima o resultado da divisão do primeiro pelo segundo. 
     #   Caso não for possível, mostre a mensagem “DIVISAO IMPOSSIVEL”.
     n = int(input('Quantos casos você vai digitar? '))
-    while n > 0:
+    for i in range(n):
         numerador = int(input('Entre com o numerador: '))
         denominador = int(input('Entre com o numerador: '))
         if denominador == 0:
@@ -242,15 +213,13 @@ def exerc14():
         else:
             divisao = numerador / denominador
             print(f'DIVISÃO {divisao:.2f}')
-        n -= 1
 
 def exerc15():
     #Exercício 15 - FATORIAL - Fazer um programa para ler um número natural N (valor máximo: 15), e depois calcular e mostrar o fatorial de N. 
     n = int(input('Digite o valor de N: '))
     fatorial = 1
-    while n > 0:
-        fatorial *= n
-        n -= 1
+    for i in range(n):
+        fatorial *= (i + 1)
     print(f'FATORIAL = {fatorial}')
 
 def exerc16():
@@ -261,10 +230,10 @@ def exerc16():
     #   indicando o tipo de cobaia (R:Rato S:Sapo C:Coelho). Apresente o total de cobaias utilizadas, o total de cada tipo de cobaia utilizada e o percentual de cada uma em relação ao total de cobaias utilizadas, 
     #   sendo que o percentual deve ser apresentado com dois dígitos após o ponto. 
     n = int(input('Quantos casos de teste serão digitados: '))
-    total_cobaias = ratos = sapos = coelhos = 0
-    while n > 0:
+    ratos = sapos = coelhos = 0
+    for i in range(n):
         cobaias = int(input('Quantidade de cobaias: '))
-        tipo = input('Tipo de cobaia: ')
+        tipo = input('Tipo de cobaia [C-Coelho | R-Rato | S-Sapo]: ').upper()
         match tipo:
             case 'R':
                 ratos += cobaias
@@ -273,7 +242,6 @@ def exerc16():
             case 'C':
                 coelhos += cobaias
         total_cobaias = ratos + sapos + coelhos
-        n -= 1
     perc_coelhos = (coelhos / total_cobaias) * 100
     perc_ratos = (ratos / total_cobaias) * 100
     perc_sapos = (sapos / total_cobaias) * 100
@@ -287,4 +255,4 @@ def exerc16():
     print(f'Percentual de sapos: {perc_sapos:.2f}')
 
 
-exerc16()
+exerc5()
